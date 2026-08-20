a0e99a217c5eb169411b399d982ae79089095100
Hai5eDFM <guillermo.rn.dfm@gmail.com>
2026-08-20 11:19:33 -0600

    debounceTime

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,9 +1,20 @@
 import { updateDisplay, displayLog } from './utils';
+import { fromEvent } from 'rxjs';
+import { map, debounceTime } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
     
     const inputBox = document.getElementById('input-box');
+    const inputSrc$ = fromEvent(inputBox, 'input').pipe(
+        map(event => event.target.value),
+        debounceTime(300) // Add debounce to limit the number of events processed
+    );
+
+    inputSrc$.subscribe(value => {
+        updateDisplay(value);
+        displayLog(`Input value: ${value}`);
+    });
 
     /** end coding */
 }
